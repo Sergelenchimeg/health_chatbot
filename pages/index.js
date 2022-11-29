b2e6e7f0b812e44f8c6e7c9f52719da99203e9ca
Sergelenchimeg <eseegii@yahoo.com>
2022-11-29 20:57:27 +0800

    some fix

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,13 @@ import axios from 'axios';
 export default function Home() {
 
   const [text, setText] = useState('')
-  const [svmResult, setSvmResult] = useState([])
-  const [dtreeResult, setDtreeResult] = useState([])
+  const [svmResultText, setSvmResultText] = useState('')
+  const [svmResultPred, setSvmResultPred] = useState('')
+  const [dtreeResultText, setDtreeResultText] = useState('')
+  const [dtreeResultPred, setDtreeResultPred] = useState('')
   const [symptoms, setSymptoms] = useState([])
+  const [loading, setLoading] = useState(false);
+  const [loaded, setLoaded] = useState(false);
 
 
   return (
@@ -16,30 +20,40 @@ export default function Home() {
     <Box>
       <Container>
         <Box display="flex" minHeight={400} pt={8}justifyContent="center" alignItems="center" width="100%" flexDirection="column">
-          <Typography variant="h5" textAlign={"center"}>Please write your text here ..</Typography>
+          <Typography variant="h5" textAlign={"center"}>Текстээ энд оруулна уу ..</Typography>
           <Box width={"100%"} maxWidth={500}>
             <Box mt={4} component="form" display="flex" justifyContent="center" width="100%">
-              <TextField sx={{fontSize: 28,}} minRows={6} label="Query" fullWidth maxRows={6} multiline onChange={(e) => setText(e.target.value)} />
+              <TextField sx={{fontSize: 28,}} minRows={8} label="Query" fullWidth maxRows={8} multiline onChange={(e) => setText(e.target.value)} />
             </Box>
             <Box mx="auto" display="flex" width="100%" mt={2} justifyContent="center">
               <Button onClick={ async () => {
+                setLoading(true)
                 var res = await getPredict(text);
-              setSymptoms([...res.extracted_symptoms])
-              setSvmResult([...res.svm.prediction[0], res.svm.mean_score[0].toString().substring(0, 4)])
-              setDtreeResult([res.decision_tree.prediction[0], res.decision_tree.mean_score[0].toString().substring(0, 4)])}} 
+                setSymptoms([...res.extracted_symptoms])
+                setSvmResultText(res.svm.prediction[0])
+                setSvmResultPred(res.svm.mean_score[0].toString().substring(0, 4))
+                setDtreeResultText(res.decision_tree.prediction[0])
+                setDtreeResultPred(res.decision_tree.mean_score[0].toString().substring(0, 4))
+                setLoading(false)
+                setLoaded(true)
+              }} 
               variant="contained" width="100%" sx={{
-                width: '80%',
-                px: 3,
-                py: 2,
-                borderRadius: 3,
-              }}>Get Results</Button>
+                width: '200px',
+                px: 2,
+                py: 1,
+                borderRadius: 15,
+              }}>Үр дүн харах</Button>
             </Box>
           </Box>
-          <Box mt={8} width="100%" justifyContent="center" alignItems="center">
-            <Typography variant="h5">SVM result - {svmResult }</Typography>
-            <Typography variant="h5">Decision Tree result - {dtreeResult }</Typography>
-            <Typography variant="h5">All symptoms - {symptoms}</Typography>
-          </Box>
+          {loading ? <Typography variant="h5"> Уншиж байна ... </Typography> : <Box></Box>}
+          {loaded && !loading ? 
+          <Box mt={8} display="flex" flexDirection="column" width="100%" justifyContent="center" alignItems="center">
+            <Typography variant="h5" >SVM алгоритмын нарийвчлал {svmResultPred} үр дүн {svmResultText} </Typography>
+            <Typography variant="h5" >Decision Tree алгоритмын {dtreeResultPred} үр дүн {dtreeResultText} </Typography>
+            <Typography variant="h5">Ялгаж авсан бүх шинж тэмдгүүд </Typography>
+            <Typography variant="h5">{symptoms}</Typography>
+          </Box> 
+          : <Box></Box> }
         </Box>
       </Container>
     </Box>
